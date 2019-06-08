679ef5d9dda2077e395c443a704b5e5560489725
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-06-08 13:14:32 -0300

    CHanges
    Layout Completly changed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,28 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
+
 import { HeaderSecondaryComponent } from "./header-secondary/header-secondary.component";
 import { ButtonHeaderSecondaryComponent } from "./button-header-secondary/button-header-secondary.component";
 import { ButtonSignupComponent } from "./button-signup/button-signup.component";
 import { SandwichMenuComponent } from "./sandwich-menu/sandwich-menu.component";
 import { MywebsitePageComponent } from './pages/mywebsite-page/mywebsite-page.component';
+import { HeaderSocialComponent } from './header-social/header-social.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    HeaderSocialComponent,
     HeaderSecondaryComponent,
     ButtonHeaderSecondaryComponent,
     ButtonSignupComponent,
     SandwichMenuComponent,
-    MywebsitePageComponent
+    MywebsitePageComponent,
+    HeaderMenuComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/header-social/header-social.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header-social',
+  templateUrl: './header-social.component.html',
+  styleUrls: ['./header-social.component.scss']
+})
+export class HeaderSocialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
