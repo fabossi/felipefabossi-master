7a05eef1432cfb8474bcd515ea3ba85ea3f7e913
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-06-09 17:38:36 -0300

    LazyLoading Implemented
    Lazy Loading was successfully implemented.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MywebsitePageComponent } from './pages/mywebsite-page/mywebsite-page.component';
 import { HeaderSecondaryComponent } from './header-secondary/header-secondary.component';
-import { CareerPageComponent } from './pages/career-page/career-page.component';
-import { AboutMePageComponent } from './pages/about-me-page/about-me-page.component';
 
 const routes: Routes = [
   {
@@ -11,16 +8,7 @@ const routes: Routes = [
     component: HeaderSecondaryComponent
   },
   {
-    path: 'websites',
-    component: MywebsitePageComponent
-  },
-  {
-    path: 'career',
-    component: CareerPageComponent
-  },
-  {
-    path: 'about-me',
-    component: AboutMePageComponent
+    path: 'pages', loadChildren: () => import(`./pages/pages.module`).then(m => m.PagesModuleComponent)
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { HeaderSecondaryComponent } from "./header-secondary/header-secondary.component";
-import { ButtonHeaderSecondaryComponent } from "./button-header-secondary/button-header-secondary.component";
-import { ButtonSignupComponent } from "./button-signup/button-signup.component";
-import { SandwichMenuComponent } from "./sandwich-menu/sandwich-menu.component";
-import { MywebsitePageComponent } from './pages/mywebsite-page/mywebsite-page.component';
+import { HeaderSecondaryComponent } from './header-secondary/header-secondary.component';
+import { ButtonHeaderSecondaryComponent } from './button-header-secondary/button-header-secondary.component';
+import { ButtonSignupComponent } from './button-signup/button-signup.component';
+import { SandwichMenuComponent } from './sandwich-menu/sandwich-menu.component';
 import { HeaderSocialComponent } from './header-social/header-social.component';
 import { HeaderMenuComponent } from './header-menu/header-menu.component';
 import { VideoBoxComponent } from './header-secondary/video-box/video-box.component';
 import { PageNotFoundComponent } from './error-pages/page-not-found/page-not-found.component';
-import { ManutComponent } from './error-pages/manut/manut.component';
-import { CareerPageComponent } from './pages/career-page/career-page.component';
-import { AboutMePageComponent } from './pages/about-me-page/about-me-page.component';
+
 
 @NgModule({
   declarations: [
@@ -25,13 +22,9 @@ import { AboutMePageComponent } from './pages/about-me-page/about-me-page.compon
     ButtonHeaderSecondaryComponent,
     ButtonSignupComponent,
     SandwichMenuComponent,
-    MywebsitePageComponent,
     HeaderMenuComponent,
     VideoBoxComponent,
-    PageNotFoundComponent,
-    ManutComponent,
-    CareerPageComponent,
-    AboutMePageComponent
+    PageNotFoundComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
