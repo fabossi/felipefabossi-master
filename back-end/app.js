4428e11807cc800ff8e7de87ce17a98958ad547d
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-06-21 14:23:34 -0300

    [FABOSSI] Amazon teste

--- back-end/app.js
+++ back-end/app.js
@@ -35,10 +35,10 @@ mongoose
 
 app.use('/api/', contactRoutes);
 
-app.use("/", express.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.use("/", express.static(path.join(__dirname, "public")));
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 app.listen(app.get('port'), '0.0.0.0', () => {
   console.log(`Server starting on => ${app.get('port')} `);
