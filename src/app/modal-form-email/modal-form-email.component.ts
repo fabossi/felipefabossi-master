baa264d3d457905a495206c51710d4c757034906
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-08-03 14:20:13 -0300

    [FABOSSI] Some important changes in subject by RXJS and some importants design implemented

--- src/app/modal-form-email/modal-form-email.component.ts
+++ src/app/modal-form-email/modal-form-email.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EmailServiceService } from '../services/email-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-modal-form-email',
   templateUrl: './modal-form-email.component.html',
   styleUrls: ['./modal-form-email.component.scss']
 })
-export class ModalFormEmailComponent implements OnInit {
+export class ModalFormEmailComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private emailService: EmailServiceService) {
+    this.subscription = this.emailService.showEmail.subscribe((isActivated) => {
+      if (isActivated) {
+        this.closeModal(isActivated);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 
   ngOnInit() {
   }
 
+  closeModal(active = true) {
+    active = !active;
+    return this.emailService.showEmail.next(active);
+  }
 }
