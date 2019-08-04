634b1d0f0dc4eade0cd2f85fe3c1c8d88949750d
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-08-03 21:48:07 -0300

    [FABOSSI]

--- src/app/header-secondary/header-secondary.component.ts
+++ src/app/header-secondary/header-secondary.component.ts
@@ -7,14 +7,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class HeaderSecondaryComponent implements OnInit, OnDestroy {
   animate = true;
-  animate_languages = false;
+
   constructor() { }
 
 
   ngOnInit() {
-    setTimeout(() => {
-      this.animate_languages = true;
-    }, 100);
+
   }
 
   ngOnDestroy() {
