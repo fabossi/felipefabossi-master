b3bf277c83adeb05b41caed436484200186928ce
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-08-03 21:10:11 -0300

    [FABOSSI] Started to make animations

--- src/app/header-secondary/header-secondary.component.ts
+++ src/app/header-secondary/header-secondary.component.ts
@@ -6,12 +6,15 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ["./header-secondary.component.scss"]
 })
 export class HeaderSecondaryComponent implements OnInit, OnDestroy {
-  animate = false;
+  animate = true;
+  animate_languages = false;
   constructor() { }
 
 
   ngOnInit() {
-
+    setTimeout(() => {
+      this.animate_languages = true;
+    }, 100);
   }
 
   ngOnDestroy() {
