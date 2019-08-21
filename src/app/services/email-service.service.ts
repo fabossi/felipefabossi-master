35814da3a73d030d11c9e00a577c91eb8e50c080
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-08-21 15:06:33 -0300

    [FABOSSI] rxjs operators commented in email-service.ts

--- src/app/services/email-service.service.ts
+++ src/app/services/email-service.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -24,24 +25,37 @@ export class EmailServiceService {
       const headers = new HttpHeaders();
       this.submitComplete.next('waiting');
       headers.append('Content-Type', 'application/json');
-      if (form != null || form !== undefined) {
-        this.http.post(this.LOCAL_URL + '/api/contact', form, { headers: headers })
-          .toPromise()
-          .then(data => {
-            if (data != null || data !== undefined) {
-              this.status = 'SUCCESS';
-              this.submitComplete.next('ready');
-              resolve(data);
-            }
-          }).catch(error => {
-            if (error) {
-              this.status = '400';
-              this.submitComplete.next('error');
-              console.error(error);
-              reject(error);
-            }
-          });
-      }
+      this.http.post(this.LOCAL_URL + '/api/contact', form, { headers: headers })
+
+        // .pipe(filter((data: any) => {
+        //   if (data.name === 'felipe') {
+        //     console.log(data);
+        //     return data;
+        //   }
+        // }))
+
+        // FILTER AND MAP OPERATORS
+
+        // .pipe(map((data: any) => {
+        //   data.forEach((value, index) => {
+        //     data[index].nome = data[index].name;
+        //     delete data[index].name;
+        //   });
+        //   console.log(data);
+        //   return data;
+        // }))
+
+        .toPromise()
+        .then(data => {
+          this.status = 'SUCCESS';
+          this.submitComplete.next('ready');
+          resolve(data);
+        }).catch(error => {
+          this.status = '400';
+          this.submitComplete.next('error');
+          console.error(error);
+          reject(error);
+        });
     }).catch(error => { console.error(error); });
   }
 
