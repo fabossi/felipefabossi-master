57ac1675f450633da4bd2b15186ae1c0b3d5822e
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2019-09-16 17:37:38 -0300

    [FABOSSI] Getting user information when its logged in

--- src/app/forms/contact-form/contact-form.component.ts
+++ src/app/forms/contact-form/contact-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
