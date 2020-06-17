8249cb87172fd3375bbe8274bbb0acc2eb486e5f
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-06-17 10:55:40 -0300

    :dizzy: aaaaaa
    aaaa

--- back-end/controllers/fileController.js
+++ back-end/controllers/fileController.js
@@ -1,21 +1,41 @@
 const fs = require('fs');
+const express = require('express');
 
 exports.createFiles = (req, res) => {
 
-  // Write File first parameter : Name of File, second one: The File Content
-
-  fs.writeFile('index.html', ``, function (err) {
-    if (err) throw res.status(500).json({ err: err });
-    res.status(201).json({ msg: 'Html File Created' })
+  fs.mkdir('files', function (err) {
+    err ? res.status(500).json({ err: err }) : res.status(200);
   });
 
-  fs.writeFile('index.js', ``, function (err) {
+  // Write File first parameter : Name of File, second one: The File Content
+
+  fs.writeFile('./files/index.html', ``, function (err) {
+    if (err) throw res.status(500).json({ err: err });
+    res.status(201).json({ msg: 'Files Created' })
+  });
+
+  fs.writeFile('./files/index.js', ``, function (err) {
     if (err) throw res.status(500).json({ err: err });
     res.status(201).json({ msg: 'JS File Created' });
   });
 
-  fs.writeFile('styles.css', ``, function (err) {
+  fs.writeFile('./files/styles.css', ``, function (err) {
     if (err) throw res.status(500).json({ err: err });
     res.status(201).json({ msg: 'CSS File Created' });
   });
 }
+
+exports.sendFiles = (req, res) => {
+  const folders = fs.readdirSync('./files');
+  const objArray = [];
+  folders.forEach((folder) => {
+    const obj = {};
+    const files = fs.readdirSync('./files' + folder);
+    obj.folder = folder;
+    obj.files = files;
+    objArray.push(obj);
+  });
+  res.render('index', { data: JSON.stringify(objArray) });
+  fs.unlink('./files');
+}
+
--- back-end/routes/files.route.js
+++ back-end/routes/files.route.js
@@ -2,4 +2,7 @@ const router = require("express").Router();
 const fileController = require('../controllers/fileController');
 
 router.post('/files', fileController.createFiles);
+router.post('/sendFile', fileController.sendFiles);
+
 module.exports = router;
+
